ce439374992c1c3d05db8edaa31596fa974dcfe2
Antti Ajanki <antti.ajanki@iki.fi>
2023-05-01 20:10:42 +0300

    Plot the distribution of domains

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,23 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+import squarify
 from pathlib import Path
 
 
 def main():
     results_path = Path('results')
 
-    plt.figure()
     plot_token_counts(results_path)
     plt.savefig(results_path / 'tokens.png')
 
-    plt.figure()
     plot_dates(results_path)
     plt.savefig(results_path / 'dates.png')
 
-    plt.figure()
     plot_languages(results_path)
     plt.savefig(results_path / 'language_detection.png')
+
+    plot_domains(results_path)
+    plt.savefig(results_path / 'domains.png')
 
 
 def plot_token_counts(results_path):
@@ -31,6 +33,7 @@ def plot_token_counts(results_path):
     print(f'Max: {df["Number of tokens"].max()}')
     print(f'Mean: {(df["Count"] * df["Number of tokens"]).sum()/df["Count"].sum():.1f}  ')
 
+    plt.figure()
     sns.histplot(df, x='Number of tokens', weights='Count', bins=25, binrange=[0, 4000])
 
 
@@ -41,6 +44,7 @@ def plot_dates(results_path):
                      parse_dates=[0],
                      names=['Date', 'Count'])
 
+    plt.figure()
     ax = sns.histplot(df, x='Date', weights='Count', bins=25)
     ax.set_title('dates')
     ax.set_xlabel('')
@@ -56,7 +60,37 @@ def plot_languages(results_path):
 
     print(f'Likely Finnish: {100 * fi_proportion:.1f} %')
 
+    plt.figure()
     sns.barplot(df, x="Detected language", y="Count")
+
+
+def plot_domains(results_path):
+    df = pd.read_csv(results_path / 'domains_annotated.tsv',
+                     sep='\t',
+                     header=None,
+                     names=['Domain', 'Count', 'Content class'])
+    df = df.sort_values(by='Count', ascending=False)
+
+    # cumulative domain size
+    plt.figure()
+    cumsum = df['Count'].cumsum()
+    cumsum = pd.concat([pd.Series([0]), cumsum])
+
+    ax = sns.lineplot(x=np.arange(len(cumsum)), y=cumsum, drawstyle='steps-pre')
+    ax.set_title('Cumulative document count per domain')
+    ax.set_ylabel('Documents')
+    ax.set_xlabel('Domain rank')
+
+    # Treemap of largest domains
+    plt.figure()
+    df2 = df.dropna()
+    df3 = df2[['Content class', 'Count']].groupby('Content class').sum().reset_index()
+    prop_pages = df2['Count'].sum()/df['Count'].sum()
+
+    palette = sns.color_palette('muted')
+    ax = squarify.plot(sizes=df3['Count'], label=df3['Content class'], color=palette)
+    ax.set_title(f'{len(df2)} largest domains ({100*prop_pages:.0f}% of pages)')
+    plt.axis('off')
 
 
 if __name__ == '__main__':
